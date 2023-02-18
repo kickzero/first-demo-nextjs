089af002f37fe8eced10b381c681a33abd947f24
kickzero <luanko01bmt>
2023-02-19 01:01:12 +0700

    first commit2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
+  experimental: {
+    appDir: true,
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
 }
 
 module.exports = nextConfig
