31f353325bfce502cac10b789d7a374aeafb3e9d
kickzero <luanko01bmt>
2023-02-19 00:53:09 +0700

    first commit2

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,1 +1,4 @@
-self.__FONT_LOADER_MANIFEST={pages:{},app:{'E:\\IT\\nextjs\\project\\first-project-nextjs\\node_modules\\@next\\font\\google\\target.css?{"path":"src\\\\app\\\\page.tsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/2aaf0723e720e8b9.p.woff2"]}};
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {}
+}
